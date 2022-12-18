188128f6e45fe8cdde472f3dce84d15b227f07da
snipergn <andrei.banita@gmail.com>
2022-12-18 19:55:19 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -5,27 +5,50 @@ import React, {Component} from 'react'
 class App extends React.Component {
   constructor(props) {
   super(props)
-  this.state = {
-    cities: []
+  this.state = { 
+    main: [],
+    coord: '',
+    isFetching: false
   }
+  this.fetchData = this.fetchData.bind(this)
 }
 
-componentDidMount() {
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=fd3b081fa5f1791533d9fa25f99be333' )
-  .then(response => response.json())
-  .then(city => this.setState({cities: city}))
-  .catch(err => console.log(err))
-}
+   fetchData = () => {
+    const that = this
+    let location = 'London';
+    let key = 'fd3b081fa5f1791533d9fa25f99be333';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + key  )
+    .then(res => {
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res)
+      that.setState({
+        main: res.main,
+        isFetching: true
+      });
+    })
+    .catch(err => console.log(err))
+
+  }
+    componentDidMount() {
+      this.fetchData()
+    }
 
   
 
 
   render() {
-  
+   let data =  Array.from(this.state.main)
   return (
     <div className="App">
     <Frontapp/>
-
+    <div>
+      {data.map(value => {
+        <p key={value}>{value.temp}</p>
+      })}
+    </div>
+    
     {/*  
     #1 Create input + Text for forecast
     #2 Add fetch from API for data.
