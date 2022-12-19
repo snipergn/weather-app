69bc2ef42282a50cd43810c11a64c6f456b403a1
snipergn <andrei.banita@gmail.com>
2022-12-19 12:32:59 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -2,63 +2,32 @@ import './App.css';
 import Frontapp from './Components/frontApp/frontapp.js'
 import React, {Component} from 'react'
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
   super(props)
-  this.state = { 
-    main: [],
-    coord: '',
-    isFetching: false
+  this.state = {
+    data: [],
+    weather: []
   }
-  this.fetchData = this.fetchData.bind(this)
 }
 
-   fetchData = () => {
-    const that = this
+   componentDidMount() {
     let location = 'London';
     let key = 'fd3b081fa5f1791533d9fa25f99be333';
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + key  )
-    .then(res => {
-      return res.json();
-    })
-    .then((res) => {
-      console.log(res)
-      that.setState({
-        main: res.main,
-        isFetching: true
-      });
-    })
-    .catch(err => console.log(err))
+    .then(res => res.json())
+    .then(res => this.setState({
+      data: res.main,
+      weather: res.weather
+    }))
 
   }
-    componentDidMount() {
-      this.fetchData()
-    }
-
-  
-
-
   render() {
-   let data =  Array.from(this.state.main)
-  return (
-    <div className="App">
-    <Frontapp/>
-    <div>
-      {data.map(value => {
-        <p key={value}>{value.temp}</p>
-      })}
-    </div>
-    
-    {/*  
-    #1 Create input + Text for forecast
-    #2 Add fetch from API for data.
-    #3 Data from Api are Current Day, Next Day, Two Days, 
-    Tree Days, Humidity, Wind Speed, Hours, Date, Year.
-    #4 Create charts for display data more beautiful.
-    */}
-    </div>
-    );
-  }
+    let array = Array.from(this.state.data)
+    return array.map((item) => {
+      <h1>{item}</h1>
+    })}
+   
 }
 
 export default App;
