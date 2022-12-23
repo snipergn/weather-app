78f07fa2ebe4b7ea1590934f76dd069b482ffaa6
snipergn <andrei.banita@gmail.com>
2022-12-23 18:13:42 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,84 @@
-import './App.css';
-import Mainside from './Components/mainside/mainside.js'
-import React, {Component} from 'react'
+import "./App.css";
+import Mainside from "./Components/mainside/mainside.js";
+import React, { Component } from "react";
 
 class App extends Component {
   constructor(props) {
-  super(props)
-  this.state = {
-    data: [],
-    weather: [],
-    locationState: 'Bucharest'
+    super(props);
+    this.state = {
+      data: [],
+      weather: [],
+      locationState: "Bucharest",
+    };
+    this.onSubmitHandler = this.onSubmitHandler.bind(this)
+    this.handleLocation = this.handleLocation.bind(this)
+    this.onChangeHandler = this.onChangeHandler.bind(this);
   }
-  this.handleNameChange = this.handleNameChange.bind(this);
-}
+  
 
-componentDidMount() {
-    let key = 'fd3b081fa5f1791533d9fa25f99be333';
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' +
-     this.state.locationState + '&appid=' + key +'&units=metric' )
-
-    .then(res => res.json())
-    .then(res => { 
-      let newData = this.state.data.concat([res.main]);
-      let newWeather = this.state.data.concat([res.weather]);
-      let filter = newData.filter(filtred => res.main === filtred )
-      this.setState({
-        data: filter,
-        weather: newWeather
-    })})
+  onChangeHandler(event){
+    this.setState({
+      locationState: event.target.value
+    })
   }
 
-
-  handleNameChange = (event) => {
-    this.setState({ locationState: event.target.value })
+  onSubmitHandler(e){
+    e.preventDefault();
+    let cityName = this.state.locationState;
+    this.handleLocation(cityName);
   }
 
+  handleLocation = (value) => {
+    let key = "fd3b081fa5f1791533d9fa25f99be333";
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        this.state.locationState +
+        "&appid=" +
+        key +
+        "&units=metric"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        let newData = this.state.data.concat([res.main]);
+        let newWeather = this.state.data.concat([res.weather]);
+        let filter = newData.filter((filtred) => res.main === filtred);
+        let filterWeather = newWeather.filter(
+          (filtred) => res.weather === filtred
+        );
+        this.setState({
+          data: filter,
+          weather: filterWeather,
+        });
+      });
+  };
+
+    componentDidMount() {
+      this.handleLocation()
+    }
   render() {
     return (
       <div className="App">
-        <Mainside 
-        data={this.state.data} 
-        name={this.state.locationState} 
-        onNameChange={this.handleNameChange}/>
+        <form on onSubmit={this.onSubmitHandler}>
+          <label for="fname">Your city is: </label>
+          <input
+            type="text"
+            id="fname"
+            name="fname"
+            onChange={this.onChangeHandler} 
+            value={this.state.locationState}
+          />
+          <input type="submit" value="Submit" />
+          <br></br>
+          <br></br>
+        </form>
+        <Mainside
+          data={this.state.data}
+          weather={this.state.weather}          
 
+        />
       </div>
-      )
-   }
+    );
+  }
 }
 
 export default App;
--- src/Components/mainside/mainside.js
+++ src/Components/mainside/mainside.js
@@ -1,52 +1,43 @@
-import React, { Component } from 'react';
-import './mainside.css';
+import React, { Component } from "react";
+import "./mainside.css";
 
-class Mainside extends Component  {
-
-  
+class Mainside extends Component {
 
   render() {
-    console.log(this.props.locationState)
-  return (     
+    return (
       <>
-      { this.props.data.map((main, index) => {
-        return (
-          <div className="container" key={index}>
-          <form>
-            <div className="input">
-              <label for="fname">Your city is:  </label>
-              <input type="text" id="fname" name="fname" 
-              onChange={this.handleNameChange} 
-              value={this.props.locationState} />
-            </div>
-          </form>
-            <div className="aboutday">
-               <p> Hours </p>
-               <p> Day </p>
-               <p> Date </p>
-               <p> Year </p>
-            </div>
-            <div className="main-items">
-              <img/>
-              <h1>The temperature is {main.temp} C</h1>
-              <p> Cloudly </p>
-            </div>
+        {this.props.data.map((main, index) => {
+          return (
+            <div className="container" key={index}>
+              <div className="aboutday">
+                <p> Hours</p>
+                <p> Day </p>
+                <p> Date </p>
+                <p> Year </p>
+              </div>
+			  { this.props.weather.map((item) => {
+				return (
+				<div id={item[0].id} className="main-items">
+                <img />
+                <h1>The temperature is {main.temp} C</h1>
+                <p> {item[0].main} </p>
+              </div>
+				)
+			  	})
+			  }
 
-            <div className="second-data">
-              <p> Humidity Level is {main.humidity}% </p> <br/>
-              <p> Wind Speed </p>
+              <div className="second-data">
+                <p> Humidity Level is {main.humidity}% </p> <br />
+                <p> Wind Speed </p>
+              </div>
+              <div className="display-second">
+                <p> 45% </p>
+                <p> 19.2 km/j</p>
+              </div>
             </div>
-            <div className="display-second">
-              <p> 45% </p>
-              <p> 19.2 km/j</p>
-            </div> 
-          </div>
-          )
-      })
-          
-        }
+          );
+        })}
       </>
-
     );
   }
 }
