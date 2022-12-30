dbf83924dc35ffeef6b478e41392363339154149
snipergn <andrei.banita@gmail.com>
2022-12-30 18:45:40 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -9,21 +9,17 @@ class App extends Component {
     this.state = {
       dataForecast: [],
       data: [],
-      weather: [],
-      wind: [],
-      icon: "",
-      iconForecast: "",
       locationState: "Bucharest",
-      lat: '',
-      long: ''
+      lat: "44.4361414",
+      lon: "26.1027202",
     };
 
     this.onSubmitHandler = this.onSubmitHandler.bind(this);
     this.handleLocation = this.handleLocation.bind(this);
     this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onLocationState = this.onLocationState.bind(this);
     this.handleGeolocation = this.handleGeolocation.bind(this);
-    this.handleForecastLocation = this.handleForecastLocation.bind(this);
-    this.handleGeolocationCity = this.handleGeolocationCity.bind(this);
+    this.onLocationState = this.onLocationState.bind(this);
   }
 
   onChangeHandler(event) {
@@ -35,110 +31,84 @@ class App extends Component {
   onSubmitHandler(e) {
     e.preventDefault();
     this.handleLocation();
-    this.handleForecastLocation()
-    this.handleGeolocationCity();
+    this.onLocationState();
   }
 
-  handleGeolocationCity = () => {
-    const key = 'fd3b081fa5f1791533d9fa25f99be333';
-    let cityname = this.state.locationState
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${key}`;
-    fetch(url)
-    .then((res) => res.json())
-    .then((res) => 
-    {   
-      let data = res[Object.keys(res)[0]];
-      this.setState({
-        lat: data.lat,
-        long: data.lon
-      })
-      console.log(this.state.lat, this.state.long)
-      
-    } )
-
-  }
-  handleForecastLocation = () => {
-    const key = 'fd3b081fa5f1791533d9fa25f99be333'
-    let cityname = this.state.locationState
-    const url =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      cityname +
-      "&appid=" +
-      key + 
-      "&units=metric"
-      + "&lang=ro";
-      fetch(url)
+  onLocationState = () => {
+    let lat = this.state.lat;
+    let lon = this.state.lon;
+    const key = "fd3b081fa5f1791533d9fa25f99be333";
+    const urlfor = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${key}&units=metric`;
+    fetch(urlfor)
       .then((res) => res.json())
       .then((res) => {
-        let forescastday = this.state.dataForecast.concat([res.list])
-        let filterforecast = forescastday.filter(filter => filter === res.list)
-        let data = filterforecast[Object.keys(filterforecast)[0]];
-
+        const current = this.state.data.concat([res.current]);
+        const filtercurrent = current.filter(
+          (filtred) => res.current === filtred
+        );
+        const daily = this.state.dataForecast.concat([res.daily]);
+        const filterdaily = daily.filter((filtred) => res.daily === filtred);
+        let objectkey = filterdaily[Object.keys(filterdaily)[0]];
 
         this.setState({
-          dataForecast: data
-        })
+          data: filtercurrent,
+          dataForecast: objectkey,
+        });
+        console.log(filtercurrent);
       })
-  }
+      .catch((error) =>
+        console.log(error, "Some error here with API Location")
+      );
+  };
 
   handleLocation = () => {
-    const key = 'fd3b081fa5f1791533d9fa25f99be333'
+    const key = "fd3b081fa5f1791533d9fa25f99be333";
     const url =
       "http://api.openweathermap.org/data/2.5/weather?q=" +
       this.state.locationState +
       "&appid=" +
       key +
-      "&units=metric"
-      + "&lang=ro";
+      "&units=metric";
     fetch(url)
       .then((res) => res.json())
       .then((res) => {
-        const newData = this.state.data.concat([res.main]);
-        const newWeather = this.state.data.concat([res.weather]);
-        const newWindRes = this.state.data.concat([res.wind]);
-        const filter = newData.filter((filtred) => res.main === filtred);
-        const filterWeather = newWeather.filter(
-          (filtred) => res.weather === filtred
-        );
-        const filtredWind = newWindRes.filter(
-          (filtred) => filtred === res.wind
-        );
-        const iconName = filterWeather.map((item) => item[0].icon);
-        const iconurl = "http://openweathermap.org/img/w/" + iconName + ".png";
+        const coord = this.state.data.concat([res.coord]);
+        const filterCoord = coord.filter((filtred) => res.coord === filtred);
         this.setState({
-          data: filter,
-          weather: filterWeather,
-          wind: filtredWind,
-          icon: iconurl,
+          lat: filterCoord[0].lat,
+          lon: filterCoord[0].lon,
         });
       })
-      .catch(error => console.log(error, "Some error here with API Location"));
+      .catch((error) =>
+        console.log(error, "Some error here with API Location")
+      );
   };
   handleGeolocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
-      const API_KEY ='fd3b081fa5f1791533d9fa25f99be333';
+      const API_KEY = "fd3b081fa5f1791533d9fa25f99be333";
       const lat = position.coords.latitude;
       const lng = position.coords.longitude;
-      const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lng}&limit=1&appid=${API_KEY}&lang=ro"`;
+      const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lng}&limit=1&appid=${API_KEY}`;
       fetch(url)
         .then((res) => {
           return res.json();
         })
         .then((res) => {
-          let locationCity = res[0].name + ', ' + res[0].country
-          console.log(res)
+          let locationCity = res[0].name + ", " + res[0].country;
           this.setState({
-            locationState: locationCity
-          })
+            locationState: locationCity,
+          });
         })
+        .catch((error) =>
+          console.log(error, "Some error here with Geolocation Finder")
+        );
     });
   };
 
   componentDidMount() {
     this.handleLocation();
     this.handleGeolocation();
-    this.handleForecastLocation();
-    this.handleGeolocationCity();
+    this.onLocationState();
   }
 
   render() {
@@ -146,17 +116,11 @@ class App extends Component {
       <div className="App">
         <Mainside
           data={this.state.data}
-          weather={this.state.weather}
-          wind={this.state.wind}
           locationState={this.state.locationState}
-          icon={this.state.icon}
           onSubmit={this.onSubmitHandler.bind(this)}
           onChangeEvent={this.onChangeHandler.bind(this)}
         />
-        <Secondside
-          dataForecast = {this.state.dataForecast}
-        />
-        
+        <Secondside dataForecast={this.state.dataForecast} />
       </div>
     );
   }
--- src/Components/secondside/secondside.js
+++ src/Components/secondside/secondside.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "./secondside.css";
-const Secondside = ({ dataForecast }) => {
+const Secondside = ({ dataForecast }) => { 
+  
+ 
   return (
-    <div className="d-flex flex-sm-row row containers ">
+
+    <div className="d-flex flex-sm-row row container-fluid mt-5 mr-5  ">
       {dataForecast.map((item, index) => {
+        const update = new Date(item.dt* 1000).toLocaleDateString('en-US')
         return (
           <div
             key={index}
-            className=" d-flex justify-content-end col-sm-2">
-                <div className="card ">
-                  <p className="mx-auto day-title">{item.dt_txt}</p>
+            className=" d-flex justify-content-end col-lg-2 gx-0 ">
+                <div className="card">
+                  <p className="mx-auto day-title">{update}</p>
                   <img
                     className="card-img-top mx-auto "
                     src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`}
                     alt="Card cap"
                   />
                   <div className="card-body ">
-                    <h3 className="card-title ">{item.main.temp}°C</h3>
+                    <h3 className="card-title ">{Math.round(item.temp.day)}°C/<span className="h4 text-secondary">{Math.round(item.temp.night)}°C</span></h3>
                     <p className="humidity">Humidity</p>
-                    <span className="humidity-item">{item.main.humidity}%</span>
+                    <span className="humidity-item">{Math.round(item.humidity)}%</span>
                   </div>
                 </div>
               </div>
